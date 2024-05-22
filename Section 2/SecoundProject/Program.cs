7361c6402ea6bf8d2c5dfb2012c07af3a4d766cc
fabinQ <maciejfabinski@gmail.com>
2024-05-22 22:43:50 +0200

    Parsowanie, listy, pętle

--- Section 2/SecoundProject/Program.cs
+++ Section 2/SecoundProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Coding.Exetcise
 {
@@ -45,24 +47,163 @@ namespace Coding.Exetcise
                     break;
                 }
                 default:
-                    result+=3;
+                    result+=5+(hours-1)*3;
                     break;
             }
-
             return result;
         }
     }
-
-
+ 
     class Program
     {
+        static void PrasePhrase(){
+            byte byteValue = 100;
+            int intValue = byteValue;
+            byte byteValue2 = (byte)intValue;
+            double doubleValue = 4.5;
+            int intValue2 = (int)doubleValue;
+            System.Console.WriteLine(intValue2);
+
+            string stringValue2 = intValue2.ToString();
+
+
+            System.Console.WriteLine("Write your year of birth");
+            string userInput = Console.ReadLine();
+            int yearOfBirth;
+            if(int.TryParse(userInput, out yearOfBirth))
+            {
+                int age = DateTime.Now.Year - yearOfBirth;
+                Console.WriteLine("You are " + age);
+            }
+            else
+            {
+                System.Console.WriteLine("Incoret value");
+                PrasePhrase();
+            }
+        }
+        static void BMIindex(){
+            System.Console.Write("BMI index ");
+            System.Console.Write("Write your weight [kg]: ");
+
+            string userInput = Console.ReadLine();
+            float weight;
+            if(float.TryParse(userInput, out weight))
+            {
+                System.Console.Write("Write your height [m]: ");
+                string userInput2 = Console.ReadLine();
+
+                float height;
+                if(float.TryParse(userInput2, out height)){
+                    float BMI = weight/(height*height);
+                    
+                    Console.WriteLine("Your BMI " + BMI);
+                    switch (BMI){
+                        case <(float)18.5:
+                        {
+                            System.Console.WriteLine("UNDERWEIGHT");
+                            break;
+                        }
+                        case <(float)24.9:
+                        {
+                            System.Console.WriteLine("NORMAL");
+                            break;
+                        }
+                        case <(float)29.9:
+                        {
+                            System.Console.WriteLine("OVERWEIGHT");
+                            break;
+                        }
+                        case <(float)34.9999:
+                        {
+                            System.Console.WriteLine("OBSESE");
+                            break;
+                        }
+                        default:
+                        {
+                            System.Console.WriteLine("EXTREMELY OBSESE");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("Incorrect value. Try again");
+                    BMIindex();
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Incoret value. Try again");
+                BMIindex();
+            }
+        }
+        
+        static void tableFunction()
+        {
+            string[] cars ={"Volvo", "BMW", "Mazda"};
+            System.Console.WriteLine(cars[0]);
+            int arrayLength = cars.Length;
+            System.Console.WriteLine("Length array " + arrayLength);
+            cars[2] = "Tesla";
+            System.Console.WriteLine(cars[2]);
+        }
+        static void whileFunction()
+        {
+            string[] cars ={"Volvo", "BMW", "Mazda"};
+
+            int i = 0;
+            while (i <=(cars.Length-1)){
+                System.Console.WriteLine(cars[i]);
+                if (cars[i] =="BMW")
+                {
+                    System.Console.WriteLine("Break");
+                    break;
+                }
+                i++;
+            System.Console.WriteLine("-----------------");
+            }
+        }
+        static void doWhileFunction()
+        {
+            System.Console.WriteLine("To exit, tap 'x'");
+            string userInput3;
+            do
+            {
+                userInput3 = Console.ReadLine();
+                System.Console.WriteLine($"Ech: {userInput3}");
+            }while (userInput3 != "x");
+            
+        }
+        static void forLoop()
+        {
+            string[] cars ={"Volvo", "BMW", "Mazda"};
+            for (int i =0; i<cars.Length;i++)
+            {
+                System.Console.WriteLine(cars[i]);
+                if (cars[i]== "BMW"){
+                    System.Console.WriteLine("Break");
+                    break;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            string result = GradeCalculator.CalculateGrade(80);
-            Console.WriteLine(result); 
+            // string result = GradeCalculator.CalculateGrade(80);
+            // Console.WriteLine(result); 
             
-            double result2 = ParkingCalculator.CalculateParkingFee(3);
-            System.Console.WriteLine(result2);
+            // double result2 = ParkingCalculator.CalculateParkingFee(3);
+            // System.Console.WriteLine(result2);
+
+            // PrasePhrase();
+
+            // BMIindex();
+
+            // tableFunction();
+
+            // whileFunction();
+            // doWhileFunction();
+            forLoop();
         }
     }
 }
