75c0fd2192622f63fc38af87700ec075481057ca
fabinQ <maciejfabinski@gmail.com>
2024-06-05 23:07:30 +0200

    Klasy

---
+++ Section 3/Klasy/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Klasy;
+
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+    private DateTime dateOfBirth;
+
+    public void SetDateOfBirth(DateTime date)
+    {
+        if (date > DateTime.Now)
+        {
+            System.Console.WriteLine("Invalid date of birth");
+        }
+        else
+        {
+            System.Console.WriteLine(date);
+            dateOfBirth = date;
+        }
+    }
+    public DateTime GetDateOfBirth() => dateOfBirth;
+
+    public void SeyHi()
+    {
+        System.Console.WriteLine($"Hi, I'm {this.FirstName}, {GetDateOfBirth()}");
+    }
+}
---
+++ Section 3/Klasy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Klasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bill = new Person();
+            bill.FirstName = "Bill";
+            bill.SetDateOfBirth(new DateTime(1999, 1, 2));
+            bill.SeyHi();
+        }
+    }
+}
