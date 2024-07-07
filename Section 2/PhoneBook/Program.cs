64c1b75d21d75b662fc2baf9e8b696e77354a0f5
fabinQ <maciejfabinski@gmail.com>
2024-07-07 23:14:45 +0200

    phone book aplication

---
+++ Section 2/PhoneBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace PhoneBook;
+    public class phoneBook
+    {
+        Dictionary<string, int> PhoneBookContact = new Dictionary<string, int>();
+        Dictionary<int, string> PhoneBookNumber = new Dictionary<int, string>();
+        public phoneBook (string contactName, int phoneNumber )
+        {
+            string ContactName = contactName;
+            int PhoneNumber = phoneNumber;
+            AddContact(ContactName, PhoneNumber);
+        }
+        private void AddContact(string ContactName, int PhoneNumber)
+        {
+            PhoneBookContact.Add(ContactName, PhoneNumber);
+            PhoneBookNumber.Add(PhoneNumber,ContactName);
+        }
+        private phoneBook FindConact(var SeekFragment)
+        {
+            
+        }
+
+
+    }
