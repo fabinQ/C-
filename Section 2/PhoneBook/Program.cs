233d2a74843d6f8020d2b1fcf0854dd2443c5896
fabinQ <maciejfabinski@gmail.com>
2024-08-17 21:00:30 +0200

    add description

--- Section 2/PhoneBook/Program.cs
+++ Section 2/PhoneBook/Program.cs
@@ -11,41 +11,46 @@ namespace PhoneBook;
         static void Main(string[] args)
         {
             System.Console.WriteLine("What do you want?");
-            System.Console.WriteLine("1. Add contact.\n2. Display contact by number.\n3. Display all contacts. \n4. Search contacts");
+            System.Console.WriteLine("1. Add contact.\n2. Display contact by number.\n3. Display all contacts. \n4. Search contacts\nTo exit press x");
             var phoneBook = new PhoneBook();
             
-            var Inputchoice = System.Console.ReadLine();
             
-            switch (Inputchoice)
+            while(true)
             {
-                case "1":
-                    System.Console.WriteLine("Insert your new contact name.");
-                    string NewContactName = System.Console.ReadLine();
+                var Inputchoice = System.Console.ReadLine();
+                switch (Inputchoice)
+                {
+                    case "1":
+                        System.Console.WriteLine("Insert your new contact name.");
+                        string NewContactName = System.Console.ReadLine();
 
-                    System.Console.WriteLine("and contact number.");
-                    string NewContactNumber = System.Console.ReadLine();
+                        System.Console.WriteLine("and contact number.");
+                        string NewContactNumber = System.Console.ReadLine();
 
-                    var newContact = new Contact(NewContactName, NewContactNumber);
-                    
-                    
-                    phoneBook.AddContact(newContact);
-                    break;
-                case "2":
-                    System.Console.WriteLine("Insert your contact.");
-                    string searchingContact = System.Console.ReadLine();
-                    phoneBook.DisplayContact(searchingContact);
-                    break;
-                case "3":
-                    phoneBook.DisplayAllContact();
-                    break;
-                case "4":
-                    System.Console.WriteLine("Insert searching fragment.");
-                    string searchingFragment = System.Console.ReadLine();
-                    phoneBook.DisplayMatchingContacts(searchingFragment);
-                    break;
-                default:
-                    System.Console.WriteLine("Invalid operation");
-                    break;
+                        var newContact = new Contact(NewContactName, NewContactNumber);
+                        
+                        phoneBook.AddContact(newContact);
+                        break;
+                    case "2":
+                        System.Console.WriteLine("Insert contactnumber.");
+                        string searchingContact = System.Console.ReadLine();
+                        phoneBook.DisplayContact(searchingContact);
+                        break;
+                    case "3":
+                        phoneBook.DisplayAllContact();
+                        break;
+                    case "4":
+                        System.Console.WriteLine("Insert searching fragment.");
+                        string searchingFragment = System.Console.ReadLine();
+                        phoneBook.DisplayMatchingContacts(searchingFragment);
+                        break;
+                    case "x":
+                        return;
+                    default:
+                        System.Console.WriteLine("Invalid operation");
+                        break;
+                }
+                System.Console.WriteLine("\nSelect operation (insert x to exit)");
             }
         
         }
