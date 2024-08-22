41c2de060272c6b663081eae07a541f60d3ad8e8
fabinQ <maciejfabinski@gmail.com>
2024-08-22 23:00:52 +0200

    add class Car and Equals function

--- Section 6/RefTypeAndValue/Program.cs
+++ Section 6/RefTypeAndValue/Program.cs
@@ -52,14 +52,47 @@ public class Program
         {
             System.Console.WriteLine($"{value} is not divided by {factor}. Remider is {reminder}");
         }
-
+        ////////////////////////////////////////////////////////////////
 
         int result;
         string input;
-        while(!TryParseToNegative (System.Console.ReadLine(), out result))
+        System.Console.WriteLine("\nInsert negative number.");
+        while(!TryParseToNegative ("-1", out result)) //System.Console.ReadLine()
         {
             System.Console.WriteLine("\nInsert negative number.");
         }
         System.Console.WriteLine($"Negative value: {result}");
+        ////////////////////////////////////////////////////////////////
+        int horsePower1= 350;
+        int horsePower2= 350;
+        bool valueTypeEquality = horsePower1 == horsePower2;
+
+        Car car1 = new Car(horsePower1);
+        Car car2 = new Car(horsePower2);
+        bool referenceTypeEquality = car1.Equals(car2);
+
+        System.Console.WriteLine($"Value Type Equality {valueTypeEquality}");
+        System.Console.WriteLine($"Reference Type Equality {referenceTypeEquality}");
+    }
+    public class Car
+    {
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            Car carObj = (Car)obj;
+            return this.HorsePower == carObj.HorsePower;
+        }
+        public Car (int horsePower)
+        {
+            HorsePower = horsePower;
+        }
+        public int HorsePower{get;set;}
     }
 }
