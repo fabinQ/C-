a4877bcf3b235fd50c495a925ea3cac026f8452c
fabinQ <maciejfabinski@gmail.com>
2024-09-29 23:41:20 +0200

    praca z plikami

---
+++ Section 8/FileProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileProject;
+
+public class Program
+{
+    static void ReadFiles()
+    {
+        var document1 = File.ReadAllText(@".\..\..\..\..\..\Section 8\Documents\document1.txt");
+        var document2 = File.ReadAllLines(@".\..\..\..\..\..\Section 8\Documents\document2.txt");
+
+        System.Console.WriteLine("document1");
+        System.Console.WriteLine(document1);
+        System.Console.WriteLine("document2");
+        System.Console.WriteLine(document2);
+    }
+    static void Main(string[] args)
+    {
+        ReadFiles();
+    }
+}
