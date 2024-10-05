d88332978dbc4a6c8883b933675e59bb49094bb9
fabinQ <maciejfabinski@gmail.com>
2024-10-05 21:47:53 +0200

    praca z plikami

--- Section 8/FileProject/Program.cs
+++ Section 8/FileProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace FileProject;
 
@@ -10,13 +11,46 @@ public class Program
         var document1 = File.ReadAllText(@".\..\..\..\..\..\Section 8\Documents\document1.txt");
         var document2 = File.ReadAllLines(@".\..\..\..\..\..\Section 8\Documents\document2.txt");
 
-        System.Console.WriteLine("document1");
-        System.Console.WriteLine(document1);
+        // System.Console.WriteLine("document1");
+        // System.Console.WriteLine(document1);
         System.Console.WriteLine("document2");
         System.Console.WriteLine(document2);
+
+        // foreach(string line in document2){
+        // System.Console.WriteLine(line);
+        // }
+        string document3 = String.Join(Environment.NewLine, document2);
+        System.Console.WriteLine(document3);
+    }
+    static void GenerateDocuments()
+    {
+        System.Console.WriteLine("Insert  name");
+        var name = Console.ReadLine();
+
+        System.Console.WriteLine("Insert  order number");
+        var orderNumber = Console.ReadLine();
+
+        var template = File.ReadAllText(@".\..\..\..\..\..\Section 8\Documents\template.txt");
+        var document = template.Replace("{name}", name)
+            .Replace("{orderNumber}", orderNumber)
+            .Replace("{dateTime}", DateTime.Now.ToString());
+
+        File.WriteAllText($@".\..\..\..\..\..\Section 8\Documents\document-{name}.txt", document);
+    }
+
+    static void ScanAndAppend()
+    {
+        var files = Directory.GetFiles(@".\..\..\..\..\..\Section 8\Documents\Append\", "*.txt", SearchOption.AllDirectories);
+        foreach (var file in files)
+        {
+            File.AppendAllText(file, String.Join(Environment.NewLine,"All right reserved"));
+        }
+
     }
     static void Main(string[] args)
     {
-        ReadFiles();
+        // ReadFiles();
+        // GenerateDocuments();
+        ScanAndAppend();
     }
 }
