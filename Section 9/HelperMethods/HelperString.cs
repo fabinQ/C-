8b2805f1cd668242d82d78cbbc91a18268217013
fabinQ <maciejfabinski@gmail.com>
2024-10-15 23:45:53 +0200

    initial new project

---
+++ Section 9/HelperMethods/HelperString.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelperMethods;
+
+public class HelperString
+{
+    public void helperMain()
+    {
+        System.Console.WriteLine("insert input:");
+        string input = System.Console.ReadLine();
+
+
+    }
+
+    public void 
+}
